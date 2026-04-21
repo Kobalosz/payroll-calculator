fbaf948857a2072cc9510c97fa6502c11f4d06c1
Kobalosz <imanuel.dartey@gmail.com>
2026-04-21 14:13:06 -0700

    Built Employee constructor with arguments.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -6,9 +6,12 @@ public class Employee {
     double hoursWorked;
     double payRate;
 
-
-    public Employee(){
-
+// Here we create our constructor so that we can pass the data from the .csv file into here and create our employee objects!
+    public Employee(int id, String name, double hours, double payRate){
+        this.employeeId = id;
+        this.fullName = name;
+        this.hoursWorked = hours;
+        this.payRate = payRate;
     }
 
 
