6d3046eae7cc8a8066e3e282b151bb8a76d5b6cd
Kobalosz <imanuel.dartey@gmail.com>
2026-04-21 14:07:11 -0700

    Initial commit

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Employee {
+    int employeeId;
+    String fullName;
+    double hoursWorked;
+    double payRate;
+
+    
+
+}
---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+// What do we need?
+//        1) Employee Class
+// What do we need to do?
+//        1) We need to read the CSV
+//        2) Convert each line into an Employee Object
+//        3) Display the information of the Employee (getEmployeeId() & getGrossPay() etc.)
+
+
+    }
+}
